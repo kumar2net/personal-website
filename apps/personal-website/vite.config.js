ac3de94dac44921fe5e1184326438709d30e02be
Anantharaman Kumar <kumar2net@gmail.com>
2025-11-14 04:38:38 +0530

    Remove topics surface and internal news redirect

--- apps/personal-website/vite.config.js
+++ apps/personal-website/vite.config.js
@@ -2,16 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+const rootNodeModules = path.resolve(__dirname, '../../node_modules')
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
+      'react-router-dom': path.join(rootNodeModules, 'react-router-dom'),
     },
   },
   optimizeDeps: {
-    include: ['react-markdown'],
+    include: ['react-markdown', 'react-router-dom'],
   },
   build: {
     chunkSizeWarningLimit: 1500,
