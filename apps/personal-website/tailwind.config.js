1a6352e8680dc0e534d3252fc3fdd31dfddaa63a
kumar2net <kumar2net@gmail.com>
2025-11-13 18:05:56 +0530

    fix1

--- apps/personal-website/tailwind.config.js
+++ apps/personal-website/tailwind.config.js
@@ -4,11 +4,8 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: ['class', '[data-mui-color-scheme="dark"]'],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
+  plugins: [],
 }
